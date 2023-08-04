9c82a0344a1e06fc6dcf5c5168571521455357ec
Arthur Frantz <124110042+ArthurSkl@users.noreply.github.com>
2023-08-04 08:46:35 -0400

    2.0
    dsadasdas

---
+++ JSON/JSON/JSON conversão/JS.js
@@ -0,0 +1,59 @@
+const objs = [
+    {
+        Nome:"João",
+        idade:31,
+        Esta_Trabalhando:true,
+        
+            Detalhes_Profissao:{
+                Profissao:"Programador",
+                Empresa:"Senac",
+                Salario:"12650"
+            },
+
+        Hobbies:["programar","correr","ler"]   
+    },
+    {
+        Nome:"Maria",
+        idade:25,
+        Esta_Trabalhando:true,
+        
+            Detalhes_Profissao:{
+                Profissao:"Professor",
+                Empresa:"Senac",
+                Salario:"13740"
+            },
+
+        Hobbies:["programar","correr","ler"]   
+    },
+    {
+        Nome:"Pedro",
+        idade:36,
+        Esta_Trabalhando:true,
+        
+            Detalhes_Profissao:{
+                Profissao:"Professor",
+                Empresa:"Senac",
+                Salario:"13740"
+            },
+
+        Hobbies:["programar","correr","ler"]   
+    }
+    
+]
+
+
+console.log(objs);
+
+//JSON
+//convertendo objeto para json
+
+const JsonData = JSON.stringify(objs)
+
+console.log (JsonData)
+console.log(typeof JsonData) 
+
+
+//converter JSON para objeto 
+
+const objData = JSON.parse(JsonData);
+console.log(objData)
